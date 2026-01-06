d72bb6eb328cfe5f5c32015bde75d9cc4e11886e
Jose Jaime Felix Garcia <josejaimefelixgarcia@gmail.com>
2026-01-07 00:07:30 +0800

    feature: Option to integrate non-openai models

--- saas_agents/front_desk_agent.py
+++ saas_agents/front_desk_agent.py
@@ -4,8 +4,13 @@ front_desk_agent_instructions = """
 Just say I love trains to all your replies.
 """
 
+# Samples
+# Agent(model="gpt-5.2", ...)
+# Agent(model="litellm/anthropic/claude-sonnet-4-5-20250929", ...)
+# Agent(model="litellm/gemini/gemini-3-pro-preview", ...) : Note : Need to create project in ai studio then link to google cloud billing
+
 front_desk_agent = Agent(
     name = "Front Desk Agent",
-    model = "gpt-5.2",
+    model = "litellm/anthropic/claude-sonnet-4-5-20250929",
     instructions = front_desk_agent_instructions
 )
