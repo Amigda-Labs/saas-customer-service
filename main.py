613a90b1bdd57244b83bfcd47b213f7b947fe889
Jose Jaime Felix Garcia <josejaimefelixgarcia@gmail.com>
2026-01-06 20:19:17 +0800

    feat: Establish Agent connection using run demo loop Summary: - Import the agent in the main file - Execute run_loop in the main file --------------------------------- - Introduced .env.example for environment variable setup. - Added python-dotenv as a dependency in pyproject.toml and uv.lock. - Updated main.py to load environment variables and run the demo loop asynchronously. - Enhanced front_desk_agent with specific instructions for responses.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
+#Load Environments
+from dotenv import load_dotenv
+load_dotenv()
+
+#Import Agent
+from saas_agents.front_desk_agent import front_desk_agent
+
+#Runner
+import asyncio 
+from agents import run_demo_loop
+
+
+
+
+async def main():
     print("Hello from saas-customer-service!")
+    await run_demo_loop(front_desk_agent)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- saas_agents/front_desk_agent.py
+++ saas_agents/front_desk_agent.py
@@ -1,5 +1,10 @@
 from agents import Agent
 
+front_desk_agent_instructions = """
+Just say I love trains to all your replies.
+"""
+
 front_desk_agent = Agent(
-    name = "Front Desk Agent"
+    name = "Front Desk Agent",
+    instructions = front_desk_agent_instructions
 )
