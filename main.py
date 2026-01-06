4e533a6fc44c7149a6db221481fbdf4a10e36f09
Jose Jaime Felix Garcia <josejaimefelixgarcia@gmail.com>
2026-01-06 22:04:32 +0800

    feature: runner option added

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ from saas_agents.front_desk_agent import front_desk_agent
 #Runner
 import asyncio 
 from agents import run_demo_loop
-
+from agents import Runner
 
 
 
 async def main():
-    print("Hello from saas-customer-service!")
-    await run_demo_loop(front_desk_agent)
+    #Using Run Demo Loop
+    #await run_demo_loop(front_desk_agent)
+    
+    #Using Runner
+    result = await Runner.run(front_desk_agent, "Hi there what do you do?")
+    print(result)
+
 
 
 if __name__ == "__main__":
